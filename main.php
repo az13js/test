d1822f8cbafa55fc8b671414c8a1d9c613762da8
az13js <1654602334@qq.com>
2019-07-01 22:44:38 +0800

    为了调试方便就暂时运行100次

--- main.php
+++ main.php
@@ -9,7 +9,10 @@ for ($i = 0; $i < 3; $i++) {
     $individuals[] = new Individual(mt_rand(), mt_rand());
 }
 
-// 个体有生命活动，这里逐个触发它们
-for ($i = 0; $i < 3; $i++) {
-    $individuals[$i]->action();
+// 生命的活动生生不息，远远不止运行100次。但是为了调试方便就暂时运行100次
+for ($j = 0; $j < 100; $j++) {
+    // 个体有生命活动，这里逐个触发它们
+    for ($i = 0; $i < 3; $i++) {
+        $individuals[$i]->action();
+    }
 }
