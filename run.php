bd0fa496a8c1ad165fb31787edaaf2700ea9fad3
az13js <1654602334@qq.com>
2019-07-28 11:39:53 +0800

    神经细胞一种实现可能性的探索

--- run.php
+++ run.php
@@ -1,8 +1,4 @@
 <?php
 require_once 'autoload.php';
-var_dump(memory_get_peak_usage());
-$x = new Algorithm\Initializers\PopulationRandomInitializer(['0','1'], 3000, 200);
-$x = $x->create();
-var_dump(gettype($x));
-file_put_contents('x.txt', serialize($x));
-var_dump(memory_get_peak_usage());
+
+"aa";
