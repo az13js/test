cd76f91c5fc01e44c310a545c5735bc08ebebe23
az13js <1654602334@qq.com>
2019-10-07 19:03:45 +0800

    测试爬数据

--- test.php
+++ test.php
@@ -1,110 +1,267 @@
 <?php
-function createNormalCurlHandle(string $address)
+/**
+ * 这个工具用来并行地获取多个链接的内容
+ *
+ * @author az13js
+ * @version 0.0.1
+ */
+class MultipleUrlContextLoader
 {
-    $handle = curl_init();
-    if (false === $handle) {
-        return false;
+    /** @var string[] 将要获取内容的URL */
+    private $urls = [];
+
+    /**
+     * @var array 与$urls对应，使用网络请求获得的远程内容。
+     *
+     * 也就是说这可能是字符串编码，也可能是二进制内容。
+     * 如果获取失败，也可能是false
+     */
+    private $contents = [];
+
+    /** @var string cookie，可以是空 */
+    private $cookie = '';
+
+    /** @var string reffer，可以是空 */
+    private $reffer = '';
+
+    /** @var string useragent，可以是空 */
+    private $userAgent = '';
+
+    /**
+     * 设置所有请求链接携带的cookie信息
+     *
+     * 不是必须设置的，设置为空字符串或者不设置，请求的时候将不会传递cookie信息。
+     *
+     * @param string $cookie 设置的cookie，格式例如"user=example; id=120; token=ABCDEFG"
+     * @return bool 设置成功返回true，不对的时候返回false。
+     * @author az13js
+     */
+    public function setCookie(string $cookie): bool
+    {
+        $this->cookie = $cookie;
+        return true;
     }
-    $setResult = curl_setopt_array($handle, [
-        CURLOPT_AUTOREFERER => true,
-        CURLOPT_COOKIESESSION => false,
-        CURLOPT_FILETIME => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_FORBID_REUSE => false,
-        CURLOPT_FRESH_CONNECT => false,
-        CURLOPT_HTTPPROXYTUNNEL => false,
-        CURLOPT_NETRC => false,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_UNRESTRICTED_AUTH => false,
-        CURLOPT_UPLOAD => false,
-        CURLOPT_VERBOSE => false,
-        CURLOPT_CONNECTTIMEOUT => 30,
-        CURLOPT_TIMEOUT => 60,
-        CURLOPT_DNS_CACHE_TIMEOUT => 1000,
-        CURLOPT_MAXCONNECTS => 65535,
-        CURLOPT_MAXREDIRS => 20,
-        /*CURLOPT_COOKIE => '',
-        CURLOPT_REFERER => '',
-        CURLOPT_USERAGENT => '',
-        CURLOPT_HTTPHEADER => [],
-        */
-        CURLOPT_URL => $address,
-    ]);
-    if (false === $setResult) {
-        curl_close($handle);
-        return false;
+
+    /**
+     * 设置所有请求链接携带的refer信息
+     *
+     * 不是必须设置的，设置为空字符串或者不设置，请求的时候将不会传递refer信息。
+     *
+     * @param string $reffer 设置的reffer
+     * @return bool 设置成功返回true，不对的时候返回false。
+     * @author az13js
+     */
+    public function setReffer(string $reffer): bool
+    {
+        $this->reffer = $reffer;
+        return true;
     }
-    return $handle;
+
+    /**
+     * 设置所有请求链接携带的user agent信息
+     *
+     * 不是必须设置的，设置为空字符串或者不设置，请求的时候将不会传递user agent信息。
+     *
+     * @param string $userAgent 设置的userAgent
+     * @return bool 设置成功返回true，不对的时候返回false。
+     * @author az13js
+     */
+    public function setUserAgent(string $userAgent): bool
+    {
+        $this->userAgent = $userAgent;
+        return true;
+    }
+
+    /**
+     * 一次性设置将要获取内容的URL
+     *
+     * @param string[] $url 一维字符串数组，URL，元素非空
+     * @return bool 设置成功返回true，当URL内容不对的时候返回false。
+     * @author az13js
+     */
+    public function setUrls(array $urls): bool
+    {
+        foreach ($urls as $url) {
+            if (!is_string($url) || empty($url)) {
+                return false;
+            }
+        }
+        $this->urls = [];
+        $this->contents = [];
+        foreach ($urls as $url) {
+            $this->urls[] = $url;
+            $this->contents[] = false;
+        }
+        return true;
+    }
+
+    /**
+     * 返回获取到的内容
+     *
+     * @return array 内容与urls对应
+     * @author az13js
+     */
+    public function getContents(): array
+    {
+        return $this->contents;
+    }
+
+    /**
+     * 执行请求资源的动作
+     *
+     * @return resource|bool CURL资源，当创建失败的时候将返回false
+     * @author az13js
+     */
+    public function loadContent()
+    {
+        $total = count($this->urls);
+
+        $handle = curl_multi_init();
+        if (false === $handle) {
+            return false;
+        }
+        curl_multi_setopt($handle, CURLMOPT_PIPELINING, 1);
+        curl_multi_setopt($handle, CURLMOPT_MAXCONNECTS, 65535);
+
+        $normalHandles = [];
+        foreach ($this->urls as $address) {
+            $normalHandle = $this->createNormalCurlHandle($address, $this->cookie, $this->reffer, $this->userAgent);
+            if (false === $normalHandle) {
+                curl_multi_close($handle);
+                return false;
+            }
+            $normalHandles[] = $normalHandle;
+
+            $addResult = curl_multi_add_handle($handle, $normalHandle);
+            if (0 !== $addResult) {
+                curl_multi_close($handle);
+                return false;
+            }
+        }
+
+        $running = 0;
+        while (true) {
+            curl_multi_exec($handle, $running);
+            if ($running > 0) {
+                curl_multi_select($handle, 60);
+            } else {
+                break;
+            }
+        }
+        $this->contents = [];
+        foreach ($normalHandles as $i => $normalHandle) {
+            $content = curl_multi_getcontent($normalHandle);
+            curl_close($normalHandle);
+            $this->contents[] = $content;
+        }
+
+        curl_multi_close($handle);
+        return true;
+    }
+
+    /**
+     * 创建并返回一个依据给定的地址生成的CURL资源，该资源是用GET方法请求的
+     *
+     * @param string $address 请求的地址
+     * @param string $cookie 可选的参数，会被设为请求携带的cookie，默认是空字符串
+     * @param string $refer 可选的参数，会被设为请求携带的refer头，默认是空字符串
+     * @param string $ua 可选的参数，会被设为请求携带的UserAgent，默认是空字符串
+     * @return resource|bool CURL资源，当创建失败的时候将返回false
+     * @author az13js
+     */
+    private function createNormalCurlHandle(string $address, string $cookie = '', string $refer = '', string $ua = '')
+    {
+        $handle = curl_init();
+        if (false === $handle) {
+            return false;
+        }
+        $setResult = curl_setopt_array($handle, [
+            CURLOPT_AUTOREFERER => true,
+            CURLOPT_COOKIESESSION => false,
+            CURLOPT_FILETIME => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_FORBID_REUSE => false,
+            CURLOPT_FRESH_CONNECT => false,
+            CURLOPT_HTTPPROXYTUNNEL => false,
+            CURLOPT_NETRC => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_UNRESTRICTED_AUTH => false,
+            CURLOPT_UPLOAD => false,
+            CURLOPT_VERBOSE => false,
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_DNS_CACHE_TIMEOUT => 1000,
+            CURLOPT_MAXCONNECTS => 65535,
+            CURLOPT_MAXREDIRS => 20,
+            /*CURLOPT_COOKIE => '',
+            CURLOPT_REFERER => '',
+            CURLOPT_USERAGENT => '',
+            CURLOPT_HTTPHEADER => [],
+            */
+            CURLOPT_URL => $address,
+        ]);
+        if (!empty($cookie)) {
+            curl_setopt($handle, CURLOPT_COOKIE, $cookie);
+        }
+        if (!empty($refer)) {
+            curl_setopt($handle, CURLOPT_REFERER, $refer);
+        }
+        if (!empty($ua)) {
+            curl_setopt($handle, CURLOPT_USERAGENT, $ua);
+        }
+        if (false === $setResult) {
+            curl_close($handle);
+            return false;
+        }
+        return $handle;
+    }
 }
 
-echo date('Y-m-d H:i:s') . ' [INFO] 开始执行' . PHP_EOL;
-
-$urls = [
-    'https://www.baidu.com',
-    'https://www.china.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://cn.bing.com',
-    'https://www.google.com',
-    'https://www.google.com',
-];
-
-$total = count($urls);
-
-$handle = curl_multi_init();
-if (false === $handle) {
-    echo date('Y-m-d H:i:s') . ' [ERROR] 初始化失败' . PHP_EOL;
-    die();
-}
-curl_multi_setopt($handle, CURLMOPT_PIPELINING, 1);
-curl_multi_setopt($handle, CURLMOPT_MAXCONNECTS, 65535);
-
-$normalHandles = [];
-foreach ($urls as $address) {
-    echo date('Y-m-d H:i:s') . ' [INFO] 添加请求 ' . $address . PHP_EOL;
-    $normalHandle = createNormalCurlHandle($address);
-    if (false === $normalHandle) {
-        echo date('Y-m-d H:i:s') . ' [ERROR] 创建资源失败' . PHP_EOL;
-        die();
-    } else {
-        echo date('Y-m-d H:i:s') . ' [INFO] 资源创建成功' . PHP_EOL;
-    }
-    $normalHandles[] = $normalHandle;
-
-    echo date('Y-m-d H:i:s') . ' [INFO] 添加到并发请求资源内' . PHP_EOL;
-    $addResult = curl_multi_add_handle($handle, $normalHandle);
-    if (0 !== $addResult) {
-        echo date('Y-m-d H:i:s') . ' [ERROR] 创建资源失败，错误内容 ' . $addResult . PHP_EOL;
-        die();
-    } else {
-        echo date('Y-m-d H:i:s') . ' [INFO] 添加成功' . PHP_EOL;
-    }
+function getData(array $urls, string $cookie, string $reffer, string $userAgent): array
+{
+    $loader = new MultipleUrlContextLoader();
+    $loader->setUrls($urls);
+    $loader->setCookie($cookie);
+    $loader->setReffer($reffer);
+    $loader->setUserAgent($userAgent);
+    $loader->loadContent();
+    return $loader->getContents();
 }
 
-echo date('Y-m-d H:i:s') . ' [INFO] 执行请求' . PHP_EOL;
-$running = 0;
-while (true) {
-    curl_multi_exec($handle, $running);
-    if ($running > 0) {
-        curl_multi_select($handle, 60);
-    } else {
-        break;
+function getText($content): array
+{
+    $result = [];
+    preg_match_all('#title="[\S|\s]*?"#i', $content, $result);
+    $results = [];
+    foreach ($result as $items) {
+        foreach ($items as $item) {
+            $row = rtrim(ltrim($item, 'title="'), '"');
+            if ('' === $row) {
+                continue;
+            }
+            $results[] = $row;
+        }
+    }
+    return $results;
+}
+
+/* 百度贴吧内容获得 */
+$tiebaName = '人工智能';
+$cookie = '';
+$userAgent = 'Mozilla/5.0 (X11; CentOS; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0';
+$reffer = 'http://tieba.baidu.com';
+$pageTotal = 100;
+
+$urls = [];
+for ($i = 0; $i < $pageTotal; $i++) {
+    $urls[] = 'http://tieba.baidu.com/f?kw=' . urlencode($tiebaName) . '&ie=utf-8&pn=' . (50 * $i);
+}
+
+foreach (getData($urls, $cookie, $reffer, $userAgent) as $id => $content) {
+    if (!$content) {
+        continue;
+    }
+    foreach (getText($content) as $str) {
+        file_put_contents($id . '.txt', $str . PHP_EOL, FILE_APPEND);
     }
 }
-
-echo date('Y-m-d H:i:s') . ' [INFO] 执行结果' . PHP_EOL;
-foreach ($normalHandles as $i => $normalHandle) {
-    $content = curl_multi_getcontent($normalHandle);
-    curl_close($normalHandle);
-    echo date('Y-m-d H:i:s') . ' [INFO] 链接 ' . $urls[$i] . PHP_EOL;
-    echo date('Y-m-d H:i:s') . ' [INFO] 返回数据大小 ' . strlen($content) . PHP_EOL;
-}
-
-curl_multi_close($handle);
-echo date('Y-m-d H:i:s') . ' [INFO] 结束' . PHP_EOL;
